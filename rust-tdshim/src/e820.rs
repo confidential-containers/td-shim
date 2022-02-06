668091a101dd59565dcb3bab12ea8c09e323c20d
Liu Jiang <gerry@linux.alibaba.com>
2022-02-06 14:08:06 +0800

    td-shim: fix a bug in e820
    Fix a bug in e820 when merging adjecent entries. Also add some test
    cases for it.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- rust-tdshim/src/e820.rs
+++ rust-tdshim/src/e820.rs
@@ -4,6 +4,7 @@
 
 use td_layout::{build_time, runtime, RuntimeMemoryLayout};
 
+// Linux BootParam supports 128 e820 entries, so...
 const MAX_E820_ENTRY: usize = 128;
 
 #[derive(Clone, Copy)]
@@ -55,6 +56,7 @@ impl E820Table {
     pub fn new() -> Self {
         Self::default()
     }
+
     pub fn add_range(&mut self, r#type: E820Type, start: u64, length: u64) {
         if self.size == MAX_E820_ENTRY {
             return;
@@ -62,7 +64,7 @@ impl E820Table {
         if self.size > 0 {
             let end_entry = &mut self.entries[self.size - 1];
             let exist_end = end_entry.addr + end_entry.size;
-            if start == exist_end + 1 && r#type as u32 == end_entry.r#type {
+            if start == exist_end && r#type as u32 == end_entry.r#type {
                 end_entry.size += length;
                 return;
             }
@@ -70,14 +72,15 @@ impl E820Table {
         self.entries[self.size] = E820Entry::new(start, length, r#type);
         self.size += 1;
     }
-    pub fn as_slice(&'_ self) -> &'_ [E820Entry] {
+
+    pub fn as_slice(&self) -> &[E820Entry] {
         &self.entries
     }
 }
 
-//TBD: Remove these
 pub fn create_e820_entries(runtime_memory: &RuntimeMemoryLayout) -> E820Table {
     let mut table = E820Table::new();
+
     table.add_range(E820Type::Memory, 0, runtime_memory.runtime_acpi_base as u64);
     table.add_range(
         E820Type::Acpi,
@@ -94,5 +97,40 @@ pub fn create_e820_entries(runtime_memory: &RuntimeMemoryLayout) -> E820Table {
         build_time::TD_SHIM_MAILBOX_BASE as u64,
         build_time::TD_SHIM_MAILBOX_SIZE as u64,
     );
+    // TODO: above memory above 4G? Should those memory be reported as `Memory`?
+
     table
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use core::mem::size_of;
+
+    #[test]
+    fn test_e820_entry_size() {
+        assert_eq!(size_of::<E820Entry>(), 20);
+        assert_eq!(
+            size_of::<[E820Entry; MAX_E820_ENTRY]>(),
+            20 * MAX_E820_ENTRY
+        );
+    }
+
+    #[test]
+    fn test_e820_table() {
+        let mut table = E820Table::new();
+        assert_eq!(table.size as usize, 0);
+        table.add_range(E820Type::Memory, 0x0, 0x1000);
+        assert_eq!(table.size as usize, 1);
+        table.add_range(E820Type::Memory, 0x1000, 0x1000);
+        assert_eq!(table.size as usize, 1);
+        assert_eq!(table.entries[0].size as u64, 0x2000);
+        table.add_range(E820Type::Acpi, 0x2000, 0x1000);
+        assert_eq!(table.size as usize, 2);
+
+        for idx in 0..MAX_E820_ENTRY {
+            table.add_range(E820Type::Memory, idx as u64 * 0x2000, 0x1000);
+        }
+        assert_eq!(table.size as usize, MAX_E820_ENTRY);
+    }
 }
