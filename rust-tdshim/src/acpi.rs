1604330ef960182502e609be9df272b2190165e6
Liu Jiang <gerry@linux.alibaba.com>
2022-02-06 11:14:02 +0800

    td-shim: refine ACPI implementation
    Refine ACPI implementation by:
    1) keep data structs private when possible
    2) follow the suggested way to calculate checksum
    3) ensure table count doesn't exceed ACPI_TABLES_MAX_NUM
    4) add helper AcpiTables::offset_to_address
    5) reserve space for XSDT and RSDP
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- rust-tdshim/src/acpi.rs
+++ rust-tdshim/src/acpi.rs
@@ -2,11 +2,11 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 extern crate alloc;
+
 use alloc::vec::Vec;
 use core::mem::size_of;
-use zerocopy::{AsBytes, FromBytes};
-
 use td_layout::memslice;
+use zerocopy::{AsBytes, FromBytes};
 
 const ACPI_TABLES_MAX_NUM: usize = 20;
 const ACPI_RSDP_REVISION: u8 = 2;
@@ -17,7 +17,7 @@ pub fn calculate_checksum(data: &[u8]) -> u8 {
 
 #[repr(packed)]
 #[derive(Default, AsBytes, FromBytes)]
-pub struct Rsdp {
+struct Rsdp {
     signature: [u8; 8],
     checksum: u8,
     oem_id: [u8; 6],
@@ -46,10 +46,13 @@ impl Rsdp {
 
     pub fn set_xsdt(&mut self, xsdt: u64) {
         self.xsdt_addr = xsdt;
+        self.checksum();
     }
 
     fn checksum(&mut self) {
-        self.checksum = calculate_checksum(&self.as_bytes()[0..19]);
+        self.checksum = 0;
+        self.extended_checksum = 0;
+        self.checksum = calculate_checksum(&self.as_bytes()[0..20]);
         self.extended_checksum = calculate_checksum(self.as_bytes());
     }
 }
@@ -90,7 +93,7 @@ impl GenericSdtHeader {
 
 #[repr(C, packed)]
 #[derive(Default, AsBytes, FromBytes)]
-pub struct Xsdt {
+struct Xsdt {
     header: GenericSdtHeader,
     tables: [u64; ACPI_TABLES_MAX_NUM],
 }
@@ -103,14 +106,19 @@ impl Xsdt {
         }
     }
 
-    pub fn add_table(&mut self, addr: usize) {
+    pub fn add_table(&mut self, addr: u64) {
         let table_num =
             (self.header.length as usize - size_of::<GenericSdtHeader>()) / size_of::<u64>();
-        self.tables[table_num] = addr as u64;
-        self.header.length += size_of::<u64>() as u32;
+        if table_num < ACPI_TABLES_MAX_NUM {
+            self.tables[table_num] = addr as u64;
+            self.header.length += size_of::<u64>() as u32;
+        } else {
+            panic!("too many ACPI tables, max {}", ACPI_TABLES_MAX_NUM);
+        }
     }
 
     pub fn checksum(&mut self) {
+        self.header.checksum(0);
         self.header.checksum(calculate_checksum(
             &self.as_bytes()[..self.header.length as usize],
         ));
@@ -121,8 +129,8 @@ impl Xsdt {
 pub struct AcpiTables {
     acpi_memory: &'static mut [u8],
     size: usize,
-    fadt: Option<usize>, // FADT offset in acpi memory
-    dsdt: Option<usize>, // DSDT offset in acpi memory
+    fadt: Option<(usize, usize)>, // FADT offset in acpi memory
+    dsdt: Option<usize>,          // DSDT offset in acpi memory
     table_offset: Vec<usize>,
 }
 
@@ -135,75 +143,74 @@ impl AcpiTables {
     }
 
     pub fn finish(&mut self) -> u64 {
-        // Create XSDT
         let mut xsdt = Xsdt::new();
 
-        // The Fixed ACPI Description Table (FADT) should
-        // always be the first table that XSDT points to.
-        //
-        if let Some(fadt) = self.fadt {
-            let fadt_header = GenericSdtHeader::read_from(
-                &self.acpi_memory[fadt..fadt + size_of::<GenericSdtHeader>()],
-            )
-            .unwrap();
-
-            // The Differentiated System Description Table (DSDT) is refered
-            // by the FADT table.
+        // The Fixed ACPI Description Table (FADT) should always be the first table in XSDT.
+        if let Some((fadt_off, fadt_len)) = self.fadt {
+            let fadt = &mut self.acpi_memory[fadt_off..fadt_off + fadt_len];
+            // The Differentiated System Description Table (DSDT) is referred by the FADT table.
             if let Some(dsdt) = self.dsdt {
-                let dsdt = dsdt as u32 + self.acpi_memory.as_ptr() as u32;
+                // Safe because DSDT is loaded in acpi_memory which is below 4G
+                let dsdt = self.offset_to_address(dsdt) as u32;
                 // The DSDT field of FADT [40..44]
-                // DSDT is loaded in acpi_memory which is below 4G
-                dsdt.write_to(&mut self.acpi_memory[fadt + 40..fadt + 44]);
+                dsdt.write_to(fadt[40..44]);
             }
 
             // Update FADT checksum
-            self.acpi_memory[fadt + 9] = 0;
-            let fadt_checksum =
-                calculate_checksum(&self.acpi_memory[fadt..fadt + fadt_header.length as usize]);
-            self.acpi_memory[fadt + 9] = fadt_checksum;
-
-            let fadt = fadt + self.acpi_memory.as_ptr() as usize;
-            xsdt.add_table(fadt);
+            fadt[9] = 0;
+            fadt[9] = calculate_checksum(fadt);
+            xsdt.add_table(self.offset_to_address(fadt_off));
         }
 
         for offset in &self.table_offset {
-            xsdt.add_table(offset + self.acpi_memory.as_ptr() as usize);
+            xsdt.add_table(self.offset_to_address(*offset));
         }
 
+        let xsdt_addr = self.offset_to_address(self.size);
         xsdt.checksum();
         xsdt.write_to(&mut self.acpi_memory[self.size..self.size + size_of::<Xsdt>()]);
-
-        //Create RSDP with XSDT addr
-        let mut rsdp = Rsdp::new(self.acpi_memory.as_ptr() as u64 + self.size as u64);
         self.size += size_of::<Xsdt>();
-        rsdp.write_to(&mut self.acpi_memory[self.size..self.size + size_of::<Rsdp>()]);
 
-        let rsdp_addr = self.acpi_memory.as_ptr() as u64 + self.size as u64;
+        let rsdp_addr = self.offset_to_address(self.size);
+        let rsdp = Rsdp::new(xsdt_addr);
+        rsdp.write_to(&mut self.acpi_memory[self.size..self.size + size_of::<Rsdp>()]);
         self.size += size_of::<Rsdp>();
-        rsdp_addr
+
+        rsdp_addr as u64
     }
 
     pub fn install(&mut self, table: &[u8]) {
-        if self.acpi_memory.len() < self.size + table.len() {
+        // Also reserve space for Xsdt and Rsdp
+        let total_size = self.size + table.len() + size_of::<Xsdt>() + size_of::<Rsdp>();
+        if self.acpi_memory.len() < total_size {
+            log::error!(
+                "ACPI content size exceeds limit 0x{:X}",
+                self.acpi_memory.len(),
+            );
+            return;
+        } else if table.len() < size_of::<GenericSdtHeader>() {
+            log::error!("ACPI table with length 0x{:X} is invalid", table.len());
             return;
         }
 
+        // Safe because we have checked buffer size.
         let header = GenericSdtHeader::read_from(&table[..size_of::<GenericSdtHeader>()]).unwrap();
 
-        if (&header.signature == b"FACP") {
-            self.fadt = Some(self.size);
-        } else if (&header.signature == b"DSDT") {
+        if &header.signature == b"FACP" {
+            self.fadt = Some((self.size, header.length as usize));
+        } else if &header.signature == b"DSDT" {
             self.dsdt = Some(self.size);
         } else {
             for offset in &self.table_offset {
+                // Safe because it's reading data from our own buffer.
                 let table_header = GenericSdtHeader::read_from(
-                    &self.acpi_memory[*offset..offset + size_of::<GenericSdtHeader>()],
+                    &self.acpi_memory[*offset..*offset + size_of::<GenericSdtHeader>()],
                 )
                 .unwrap();
                 if table_header.signature == header.signature {
                     log::info!(
                         "ACPI: {} has been installed, use first\n",
-                        core::str::from_utf8(&header.signature).unwrap()
+                        core::str::from_utf8(&header.signature).unwrap_or_default()
                     );
                     return;
                 }
@@ -213,5 +220,9 @@ impl AcpiTables {
 
         self.acpi_memory[self.size..self.size + table.len()].copy_from_slice(table);
         self.size += table.len();
+    }
+
+    fn offset_to_address(&self, offset: usize) -> u64 {
+        (self.acpi_memory.as_ptr() as usize + offset) as u64
     }
 }
