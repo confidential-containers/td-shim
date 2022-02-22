aa7a414b524aa578a500a75a53135fe4a3222ee3
haowx <weix.hao.intel.com>
2022-02-23 04:31:08 +0800

    Add libfuzzer
    Signed-off-by: haowx <weix.hao.intel.com>

---
+++ uefi-pi/fuzz/fuzz_targets/fv_parser.rs
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+
+use uefi_pi::{fv, pi};
+
+fn fuzz_fv_parser(data: &[u8]) {
+    let res = fv::get_image_from_fv(data, pi::fv::FV_FILETYPE_DXE_CORE, pi::fv::SECTION_PE32);
+    println!("{:?}", res.unwrap_or_default().len());
+}
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    fuzz_fv_parser(data);
+});
