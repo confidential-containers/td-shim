fb483f9aebd32c98d0be7d779d11967bf579d494
Jiaqi Gao <jiaqi.gao@intel.com>
2022-02-24 02:49:07 +0800

    td-shim: add relocated AP function
    APs are running in the loop waiting for BSP's instruction after they
    switch to long mode. At before, We assumed that CPU index get from TDCALL
    is equal to the APIC ID, but these two value may be different in some
    cases, such as configuring multiple sockets in QEMU.
    
    One solution is to relocate APs' running loop into a new place where they
    use APIC ID to identify themselves. APs will use TDVMCALL to know their
    APIC ID and wait for the instructions from BSP to wakeup. Two methods are
    introduced to get vector relocation information:
    
        - ap_relocated_func() will not be called by BSP, it is used by BSP
          to locate the address of the first instruction.
        - ap_relocated_func_size() is used to get the size of the new ap
          loop which will be copied into Mailbox memory by BSP.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- td-shim/src/bin/td-shim/asm/mod.rs
+++ td-shim/src/bin/td-shim/asm/mod.rs
@@ -1,6 +1,11 @@
-// Copyright (c) 2020 Intel Corporation
+// Copyright (c) 2020-2022 Intel Corporation
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
-
 global_asm!(include_str!("switch_stack.asm"));
 global_asm!(include_str!("msr64.asm"));
+global_asm!(include_str!("ap_loop.asm"));
+
+extern "win64" {
+    pub fn ap_relocated_func_size(size: *mut u64);
+    pub fn ap_relocated_func();
+}
