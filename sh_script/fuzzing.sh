ffd109c2987c680a6dfa096ebcf871bb648ad8ee
haowei <WeiX.Hao@intel.com>
2022-03-23 05:40:01 +0800

    sh_script: modify fuzzing.sh
    Signed-off-by: haowei <WeiX.Hao@intel.com>

--- sh_script/fuzzing.sh
+++ sh_script/fuzzing.sh
@@ -5,24 +5,13 @@ if [[ ! $PWD =~ td-shim$ ]]; then
 fi
 
 pkill screen
+
+cmds=(
+    "td-loader"
+    "td-uefi-pi"
+)
+
 afl() {
-    if [ ! -d "fuzzing/out" ]; then
-        mkdir fuzzing/out
-    fi
-
-    for i in fuzzing/out/*; do
-        echo $i
-        if [[ -f $i/default/crashes ]]; then
-            break
-        fi
-
-        if [[ "$(ls -A $i/default/crashes)" != "" ]]; then
-            echo -e "\033[31m There are some crashes \033[0m"
-            echo -e "\033[31m Path in fuzz-target/out/$i/default/crashes \033[0m"
-            exit
-        fi
-    done
-
     if [ "core" != $(cat /proc/sys/kernel/core_pattern) ]; then
         if [ $(id -u) -ne 0 ]; then
             if [[ $PWD =~ rust-td$ ]]; then
@@ -40,70 +29,42 @@ afl() {
         fi
     fi
 
-    rm -rf fuzzing/out/*
-    cmds=(
-        "fuzz_elf_loader"
-        "fuzz_pe_loader"
-        "fuzz_fv_parser"
-        "fuzz_hob_parser"
-    )
-
-    buildpackage=''
     for i in ${cmds[@]}; do
-        buildpackage="-p $i $buildpackage"
+        pushd $i/fuzz
+        fuzz_list=$(ls fuzz_targets | grep afl | cut -d. -f1)
+        for j in $fuzz_list; do
+            screen -ls | grep $j
+            if [[ $? -ne 0 ]]; then
+                screen -dmS $j
+            fi
+            if [[ ! -d artifacts/$j ]]; then
+                mkdir artifacts/$j
+            fi
+            if [[ "$(ls -A artifacts/$j/default/crashes)" != "" ]]; then
+                echo echo -e "\033[31m There are some crashes \033[0m"
+                echo -e "\033[31m Path in $i/fuzz/artifacts/$j/default/crashes \033[0m"
+            fi
+            cargo afl build --bin $j --features fuzz --no-default-features
+            seed=$(echo $j | cut -d_ -f2)
+            screen -x -S $j -p 0 -X stuff "cargo afl fuzz -i seeds/$seed -o artifacts/$j target/debug/$j"
+            screen -x -S $j -p 0 -X stuff $'\n'
+            sleep 10
+            screen -S $j -X quit
+            sleep 5
+        done
+        popd
     done
-
-    echo "cargo afl build --features fuzz $buildpackage"
-
-    if [[ $1 = "S" ]]; then
-        echo "$1coverage"
-        export RUSTFLAGS="-Zinstrument-coverage"
-        export LLVM_PROFILE_FILE='fuzz_run%m.profraw'
-    fi
-
-    if [[ $1 = "G" ]]; then
-        echo "$1coverage"
-        export CARGO_INCREMENTAL=0
-        export RUSTDOCFLAGS="-Cpanic=abort"
-        export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
-    fi
-
-    cargo afl build --features fuzz $buildpackage
-
-    for ((i = 0; i < ${#cmds[*]}; i++)); do
-        echo ${cmds[$i]}
-        screen -ls | grep ${cmds[$i]}
-        if [[ $? -ne 0 ]]; then
-            screen -dmS ${cmds[$i]}
-        fi
-        screen -x -S ${cmds[$i]} -p 0 -X stuff "cargo afl fuzz -i fuzzing/in/${cmds[$i]} -o fuzzing/out/${cmds[$i]} target/debug/${cmds[$i]}"
-        screen -x -S ${cmds[$i]} -p 0 -X stuff $'\n'
-        sleep 3600
-        screen -S ${cmds[$i]} -X quit
-        sleep 5
-    done
-
-    if [[ $1 = "S" || $1 = "G" ]]; then
-        echo "$1coverage"
-        grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/fuzz_coverage/
-        unset RUSTFLAGS
-        unset LLVM_PROFILE_FILE
-        unset CARGO_INCREMENTAL
-        unset RUSTDOCFLAGS
-        unset RUSTFLAGS
-        git clean -xf *.profraw
-    fi
 }
 
 libfuzzer() {
-    cmds=(
-        "td-loader"
-        "td-uefi-pi"
-    )
+
     for i in ${cmds[@]}; do
         pushd $i
         fuzz_list=$(cargo fuzz list)
         for j in $fuzz_list; do
+            if [[ $j =~ "afl" ]]; then
+                continue
+            fi
             if [ ! -d "fuzz/corpus/$j" ]; then
                 mkdir -p fuzz/corpus/$j
             fi
@@ -123,12 +84,6 @@ libfuzzer() {
 }
 
 case "${1:-}" in
-afl)
-    case "${2:-}" in
-    S) afl S ;;
-    G) afl G ;;
-    *) afl ;;
-    esac
-    ;;
+afl) afl ;;
 *) libfuzzer ;;
 esac
