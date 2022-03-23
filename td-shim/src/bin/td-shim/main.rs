46dddfeb7cba4d09002755e6a5d1cab145a1b842
haowei <WeiX.Hao@intel.com>
2022-03-23 22:06:14 +0800

    td-shim: Migration acpi
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    
    Signed-off-by: haowei <WeiX.Hao@intel.com>

---
+++ td-shim/src/bin/td-shim/main.rs
@@ -0,0 +1,53 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+#![allow(unused)]
+#![feature(global_asm)]
+#![feature(asm)]
+#![feature(alloc_error_handler)]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![allow(unused_imports)]
+
+use core::ffi::c_void;
+use core::mem::size_of;
+use core::panic::PanicInfo;
+
+use r_efi::efi;
+use scroll::{Pread, Pwrite};
+use zerocopy::{AsBytes, ByteSlice, FromBytes};
+
+use td_layout::build_time::{self, *};
+use td_layout::memslice;
+use td_layout::runtime::{self, *};
+use td_layout::RuntimeMemoryLayout;
+use td_shim::acpi::GenericSdtHeader;
+use td_shim::event_log::{
+    self, TdHandoffTable, TdHandoffTablePointers, EV_EFI_HANDOFF_TABLES2, EV_PLATFORM_CONFIG_FLAGS,
+    TD_LOG_EFI_HANDOFF_TABLE_GUID,
+};
+use td_shim::{
+    HobTemplate, PayloadInfo, TdKernelInfoHobType, TD_ACPI_TABLE_HOB_GUID, TD_KERNEL_INFO_HOB_GUID,
+};
+use td_uefi_pi::{fv, hob, pi};
+
+use crate::tcg::TdEventLog;
+
+mod acpi;
+
+#[cfg(not(test))]
+#[panic_handler]
+#[allow(clippy::empty_loop)]
+fn panic(_info: &PanicInfo) -> ! {
+    log::info!("panic ... {:?}\n", _info);
+    panic!("deadloop");
+}
+
+#[cfg(not(test))]
+#[alloc_error_handler]
+#[allow(clippy::empty_loop)]
+fn alloc_error(_info: core::alloc::Layout) -> ! {
+    log::info!("alloc_error ... {:?}\n", _info);
+    panic!("deadloop");
+}
