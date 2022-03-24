887e139d0c01d7c804652cca01611c684bca6b54
haowei <WeiX.Hao@intel.com>
2022-03-24 17:14:18 +0800

    td-shim: add feature cet_ss and ipl and stack_guard
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    
    Signed-off-by: haowei <WeiX.Hao@intel.com>

--- td-shim/src/bin/td-shim/main.rs
+++ td-shim/src/bin/td-shim/main.rs
@@ -46,6 +46,8 @@ mod stack_guard;
 mod tcg;
 mod td;
 
+#[cfg(feature = "cet-ss")]
+mod cet_ss;
 #[cfg(feature = "secure-boot")]
 mod verifier;
 
@@ -156,6 +158,36 @@ pub extern "win64" fn _start(
         payload = secure_boot_verify_payload(payload, &mut td_event_log);
     }
 
+    let (entry, basefw, basefwsize) =
+        ipl::find_and_report_entry_point(&mut mem, payload).expect("Entry point not found!");
+    let entry = entry as usize;
+
+    // Initialize the stack to run the image
+    stack_guard::stack_guard_enable(&mut mem);
+    #[cfg(feature = "cet-ss")]
+    cet_ss::enable_cet_ss(
+        runtime_memory_layout.runtime_shadow_stack_base,
+        runtime_memory_layout.runtime_shadow_stack_top,
+    );
+    let stack_top =
+        (runtime_memory_layout.runtime_stack_base + TD_PAYLOAD_STACK_SIZE as u64) as usize;
+
+    // Prepare the HOB list to run the image
+    let hob_base = prepare_hob_list(
+        hob_list,
+        &runtime_memory_layout,
+        basefw,
+        basefwsize,
+        memory_top_below_4gb,
+    );
+
+    // Finally let's switch stack and jump to the image entry point...
+    log::info!(
+        " start launching payload {:p} and switch stack {:p}...\n",
+        entry as *const usize,
+        stack_top as *const usize
+    );
+    unsafe { switch_stack_call(entry, stack_top, hob_base as usize, 0) };
     panic!("payload entry() should not return here, deadloop!!!");
 }
 
@@ -284,6 +316,208 @@ fn prepare_acpi_tables(
     acpi_tables.install(tdel.as_bytes());
 
     acpi_tables.finish()
+}
+
+fn prepare_hob_list(
+    hob_list: &[u8],
+    layout: &RuntimeMemoryLayout,
+    basefw: u64,
+    basefwsize: u64,
+    memory_top_below_4gb: u64,
+) -> u64 {
+    let hob_base = layout.runtime_hob_base;
+    let memory_bottom = layout.runtime_memory_bottom;
+
+    let handoff_info_table = pi::hob::HandoffInfoTable {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_HANDOFF,
+            length: size_of::<pi::hob::HandoffInfoTable>() as u16,
+            reserved: 0,
+        },
+        version: 9u32,
+        boot_mode: pi::boot_mode::BOOT_WITH_FULL_CONFIGURATION,
+        efi_memory_top: memory_top_below_4gb,
+        efi_memory_bottom: memory_bottom,
+        efi_free_memory_top: memory_top_below_4gb,
+        efi_free_memory_bottom: memory_bottom
+            + ipl::efi_page_to_size(ipl::efi_size_to_page(size_of::<HobTemplate>() as u64)),
+        efi_end_of_hob_list: hob_base + size_of::<HobTemplate>() as u64,
+    };
+
+    let cpu = pi::hob::Cpu {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_CPU,
+            length: size_of::<pi::hob::Cpu>() as u16,
+            reserved: 0,
+        },
+        size_of_memory_space: memory::cpu_get_memory_space_size(),
+        size_of_io_space: 16u8,
+        reserved: [0u8; 6],
+    };
+
+    let firmware_volume = pi::hob::FirmwareVolume {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_FV,
+            length: size_of::<pi::hob::FirmwareVolume>() as u16,
+            reserved: 0,
+        },
+        base_address: TD_SHIM_PAYLOAD_BASE as u64,
+        length: TD_SHIM_PAYLOAD_SIZE as u64,
+    };
+
+    const MEMORY_ALLOCATION_STACK_GUID: efi::Guid = efi::Guid::from_fields(
+        0x4ED4BF27,
+        0x4092,
+        0x42E9,
+        0x80,
+        0x7D,
+        &[0x52, 0x7B, 0x1D, 0x00, 0xC9, 0xBD],
+    );
+    let stack = pi::hob::MemoryAllocation {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_MEMORY_ALLOCATION,
+            length: size_of::<pi::hob::MemoryAllocation>() as u16,
+            reserved: 0,
+        },
+        alloc_descriptor: pi::hob::MemoryAllocationHeader {
+            name: *MEMORY_ALLOCATION_STACK_GUID.as_bytes(),
+            memory_base_address: layout.runtime_stack_base,
+            memory_length: TD_PAYLOAD_STACK_SIZE as u64
+                - (stack_guard::STACK_GUARD_PAGE_SIZE + stack_guard::STACK_EXCEPTION_PAGE_SIZE)
+                    as u64,
+            memory_type: efi::MemoryType::BootServicesData as u32,
+            reserved: [0u8; 4],
+        },
+    };
+
+    // Enable host Paging
+    const PAGE_TABLE_NAME_GUID: efi::Guid = efi::Guid::from_fields(
+        0xF8E21975,
+        0x0899,
+        0x4F58,
+        0xA4,
+        0xBE,
+        &[0x55, 0x25, 0xA9, 0xC6, 0xD7, 0x7A],
+    );
+
+    let page_table = pi::hob::MemoryAllocation {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_MEMORY_ALLOCATION,
+            length: size_of::<pi::hob::MemoryAllocation>() as u16,
+            reserved: 0,
+        },
+        alloc_descriptor: pi::hob::MemoryAllocationHeader {
+            name: *PAGE_TABLE_NAME_GUID.as_bytes(),
+            memory_base_address: TD_PAYLOAD_PAGE_TABLE_BASE,
+            memory_length: td_paging::PAGE_TABLE_SIZE as u64,
+            memory_type: efi::MemoryType::BootServicesData as u32,
+            reserved: [0u8; 4],
+        },
+    };
+
+    let lowmemory = hob::get_system_memory_size_below_4gb(hob_list).unwrap();
+
+    let memory_above_1m = pi::hob::ResourceDescription {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_RESOURCE_DESCRIPTOR,
+            length: size_of::<pi::hob::ResourceDescription>() as u16,
+            reserved: 0,
+        },
+        owner: *efi::Guid::from_fields(
+            0x4ED4BF27,
+            0x4092,
+            0x42E9,
+            0x80,
+            0x7D,
+            &[0x52, 0x7B, 0x1D, 0x00, 0xC9, 0xBD],
+        )
+        .as_bytes(),
+        resource_type: pi::hob::RESOURCE_SYSTEM_MEMORY,
+        resource_attribute: pi::hob::RESOURCE_ATTRIBUTE_PRESENT
+            | pi::hob::RESOURCE_ATTRIBUTE_INITIALIZED
+            | pi::hob::RESOURCE_ATTRIBUTE_UNCACHEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_WRITE_COMBINEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_TESTED,
+        physical_start: 0x100000u64,
+        resource_length: lowmemory - 0x100000u64,
+    };
+
+    let memory_below_1m = pi::hob::ResourceDescription {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_RESOURCE_DESCRIPTOR,
+            length: size_of::<pi::hob::ResourceDescription>() as u16,
+            reserved: 0,
+        },
+        owner: *efi::Guid::from_fields(
+            0x4ED4BF27,
+            0x4092,
+            0x42E9,
+            0x80,
+            0x7D,
+            &[0x52, 0x7B, 0x1D, 0x00, 0xC9, 0xBD],
+        )
+        .as_bytes(),
+        resource_type: pi::hob::RESOURCE_SYSTEM_MEMORY,
+        resource_attribute: pi::hob::RESOURCE_ATTRIBUTE_PRESENT
+            | pi::hob::RESOURCE_ATTRIBUTE_INITIALIZED
+            | pi::hob::RESOURCE_ATTRIBUTE_UNCACHEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_WRITE_COMBINEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE
+            | pi::hob::RESOURCE_ATTRIBUTE_TESTED,
+        physical_start: 0u64,
+        resource_length: 0x80000u64 + 0x20000u64,
+    };
+
+    const PAYLOAD_NAME_GUID: efi::Guid = efi::Guid::from_fields(
+        0x6948d4a,
+        0xd359,
+        0x4721,
+        0xad,
+        0xf6,
+        &[0x52, 0x25, 0x48, 0x5a, 0x6a, 0x3a],
+    );
+
+    let payload = pi::hob::MemoryAllocation {
+        header: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_MEMORY_ALLOCATION,
+            length: size_of::<pi::hob::MemoryAllocation>() as u16,
+            reserved: 0,
+        },
+        alloc_descriptor: pi::hob::MemoryAllocationHeader {
+            name: *PAYLOAD_NAME_GUID.as_bytes(),
+            memory_base_address: basefw,
+            memory_length: ipl::efi_page_to_size(ipl::efi_size_to_page(basefwsize)),
+            memory_type: efi::MemoryType::BootServicesCode as u32,
+            reserved: [0u8; 4],
+        },
+    };
+
+    let hob_template = HobTemplate {
+        handoff_info_table,
+        firmware_volume,
+        cpu,
+        payload,
+        page_table,
+        stack,
+        memory_above_1m,
+        memory_blow_1m: memory_below_1m,
+        end_off_hob: pi::hob::Header {
+            r#type: pi::hob::HOB_TYPE_END_OF_HOB_LIST,
+            length: size_of::<pi::hob::Header>() as u16,
+            reserved: 0,
+        },
+    };
+
+    // Safe because we are the only consumer.
+    let hob_slice = unsafe {
+        memslice::get_dynamic_mem_slice_mut(memslice::SliceType::PayloadHob, hob_base as usize)
+    };
+    let _res = hob_slice.pwrite(hob_template, 0);
+
+    hob_base
 }
 
 #[cfg(feature = "secure-boot")]
