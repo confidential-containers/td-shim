72d5adc7c1bc4bd859285a9402b3fc8217983823
haowei <WeiX.Hao@intel.com>
2022-03-24 16:15:42 +0800

    td-shim: add head::init
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    
    Signed-off-by: haowei <WeiX.Hao@intel.com>

---
+++ td-shim/src/bin/td-shim/heap.rs
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+use linked_list_allocator::LockedHeap;
+use td_layout::build_time::{TD_SHIM_TEMP_HEAP_BASE, TD_SHIM_TEMP_HEAP_SIZE};
+
+#[cfg(not(test))]
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+/// Initialize the heap allocator.
+pub(super) fn init() {
+    let heap_start = TD_SHIM_TEMP_HEAP_BASE as usize;
+    let heap_size = TD_SHIM_TEMP_HEAP_SIZE as usize;
+
+    unsafe {
+        #[cfg(not(test))]
+        HEAP_ALLOCATOR.lock().init(heap_start, heap_size);
+    }
+    log::info!(
+        "Heap allocator init done: {:#x?}\n",
+        heap_start..heap_start + heap_size
+    );
+}
--- td-shim/src/bin/td-shim/main.rs
+++ td-shim/src/bin/td-shim/main.rs
@@ -36,8 +36,13 @@ use crate::tcg::TdEventLog;
 
 mod acpi;
 mod asm;
+mod heap;
 mod linux;
 mod td;
+
+extern "win64" {
+    fn switch_stack_call(entry_point: usize, stack_top: usize, P1: usize, P2: usize);
+}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -53,4 +58,38 @@ fn panic(_info: &PanicInfo) -> ! {
 fn alloc_error(_info: core::alloc::Layout) -> ! {
     log::info!("alloc_error ... {:?}\n", _info);
     panic!("deadloop");
+}
+
+/// Main entry point of the td-shim, and the bootstrap code should jump here.
+///
+/// The bootstrap should prepare the context to satisfy `_start()`'s expectation:
+/// - the memory is in 1:1 identity mapping mode with paging enabled
+/// - the stack is ready for use
+///
+/// # Arguments
+/// - `boot_fv`: pointer to the boot firmware volume
+/// - `top_of_start`: top address of the stack
+/// - `init_vp`: [31:0] TDINITVP - Untrusted Configuration
+/// - `info`: [6:0] CPU supported GPA width, [7:7] 5 level page table support, [23:16] VCPUID,
+///           [32:24] VCPU_Index
+#[cfg(not(test))]
+#[no_mangle]
+#[export_name = "efi_main"]
+pub extern "win64" fn _start(
+    boot_fv: *const c_void,
+    top_of_stack: *const c_void,
+    init_vp: *const c_void,
+    info: usize,
+) -> ! {
+    // The bootstrap code has setup the stack, but only the stack is available now...
+    let _ = td_logger::init();
+    log::info!("Starting RUST Based TdShim boot_fv - {:p}, Top of stack - {:p}, init_vp - {:p}, info - 0x{:x} \n",
+               boot_fv, top_of_stack, init_vp, info);
+    td_exception::setup_exception_handlers();
+    log::info!("setup_exception_handlers done\n");
+
+    // First initialize the heap allocator so that we have a normal rust world to live in...
+    heap::init();
+
+    panic!("Linux kernel should not return here!!!");
 }
