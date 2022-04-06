2b5700fe3cd7b77de34636735cf6839e93803b07
Min Xu <min.m.xu@intel.com>
2022-04-06 16:10:04 +0800

    td-layout: Refine TdxMetadata
    TdxMetadata is refined with below changes:
    1. TdxMetadataGuid is defined with td_uefi_pi::pi::guid::Guid.
    2. Add from_bytes/is_valid functions in TdxMetadata/TdxMetadataSection.
    3. Add TdxMetadata related constants.
    
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- td-layout/src/metadata.rs
+++ td-layout/src/metadata.rs
@@ -2,15 +2,26 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 
-use core::ptr::slice_from_raw_parts;
-use scroll::{Pread, Pwrite};
+extern crate alloc;
 
-const TDX_METADATA_GUID1: u32 = 0xe9eaf9f3;
-const TDX_METADATA_GUID2: u32 = 0x44d5168e;
-const TDX_METADATA_GUID3: u32 = 0x4d7feba8;
-const TDX_METADATA_GUID4: u32 = 0xaef63887;
+use alloc::string::String;
+use core::{convert::TryInto, ptr::slice_from_raw_parts, str::FromStr};
+use scroll::{Pread, Pwrite};
+use td_uefi_pi::pi::guid::Guid;
+
+const TDX_METADATA_GUID_STR: &str = "F3F9EAE9-8E16-D544-A8EB-7F4D8738F6AE";
 
 const TDX_METADATA_SIGNATURE: u32 = 0x46564454;
+
+/// TdxMetadata Offset
+pub const TDX_METADATA_OFFSET: u32 = 0x20;
+
+/// TdxMetadata guid length
+pub const TDX_METADATA_GUID_LEN: u32 = 16;
+/// TdxMetadata description length
+pub const TDX_METADATA_DESCRIPTOR_LEN: u32 = 16;
+/// TdxMetadata section length
+pub const TDX_METADATA_SECTION_LEN: u32 = 32;
 
 /// Section type for EFI Boot Firmware Volume.
 pub const TDX_METADATA_SECTION_TYPE_BFV: u32 = 0;
@@ -20,10 +31,24 @@ pub const TDX_METADATA_SECTION_TYPE_CFV: u32 = 1;
 pub const TDX_METADATA_SECTION_TYPE_TD_HOB: u32 = 2;
 /// Section type for stack, heap and mailbox.
 pub const TDX_METADATA_SECTION_TYPE_TEMP_MEM: u32 = 3;
+/// Section type for PermMem
+pub const TDX_METADATA_SECTION_TYPE_PERM_MEM: u32 = 4;
 /// Section type for kernel image.
 pub const TDX_METADATA_SECTION_TYPE_PAYLOAD: u32 = 5;
 /// Section type for kernel parameters.
 pub const TDX_METADATA_SECTION_TYPE_PAYLOAD_PARAM: u32 = 6;
+/// Max Section type
+pub const TDX_METADATA_SECTION_TYPE_MAX: u32 = 7;
+
+pub const TDX_METADATA_SECTION_TYPE_STRS: [&str; TDX_METADATA_SECTION_TYPE_MAX as usize] = [
+    "BFV",
+    "CFV",
+    "TD_HOB",
+    "TempMem",
+    "PermMem",
+    "Payload",
+    "PayloadParam",
+];
 
 /// Attribute flags for BFV.
 pub const TDX_METADATA_ATTRIBUTES_EXTENDMR: u32 = 0x00000001;
@@ -67,6 +92,22 @@ impl TdxMetadataDescriptor {
             || (len - 16) % 32 != 0
             || (len - 16) / 32 != self.number_of_section_entry)
     }
+
+    pub fn from_bytes(buffer: &[u8; 16]) -> Option<TdxMetadataDescriptor> {
+        let mut metadata_descriptor = TdxMetadataDescriptor::default();
+
+        metadata_descriptor.signature = u32::from_le_bytes(buffer[..4].try_into().unwrap());
+        metadata_descriptor.length = u32::from_le_bytes(buffer[4..8].try_into().unwrap());
+        metadata_descriptor.version = u32::from_le_bytes(buffer[8..12].try_into().unwrap());
+        metadata_descriptor.number_of_section_entry =
+            u32::from_le_bytes(buffer[12..].try_into().unwrap());
+
+        if metadata_descriptor.is_valid() {
+            Some(metadata_descriptor)
+        } else {
+            return None;
+        }
+    }
 }
 
 #[repr(C)]
@@ -80,32 +121,125 @@ pub struct TdxMetadataSection {
     pub attributes: u32,
 }
 
+impl TdxMetadataSection {
+    pub fn get_type_name(r#type: u32) -> Option<String> {
+        if r#type >= TDX_METADATA_SECTION_TYPE_MAX {
+            None
+        } else {
+            Some(String::from(
+                TDX_METADATA_SECTION_TYPE_STRS[r#type as usize],
+            ))
+        }
+    }
+
+    pub fn from_bytes(buffer: &[u8; 32]) -> Option<TdxMetadataSection> {
+        let mut metadata_section = TdxMetadataSection::default();
+        metadata_section.data_offset = u32::from_le_bytes(buffer[..4].try_into().unwrap());
+        metadata_section.raw_data_size = u32::from_le_bytes(buffer[4..8].try_into().unwrap());
+        metadata_section.memory_address = u64::from_le_bytes(buffer[8..16].try_into().unwrap());
+        metadata_section.memory_data_size = u64::from_le_bytes(buffer[16..24].try_into().unwrap());
+        metadata_section.r#type = u32::from_le_bytes(buffer[24..28].try_into().unwrap());
+        metadata_section.attributes = u32::from_le_bytes(buffer[28..32].try_into().unwrap());
+
+        if metadata_section.is_valid() {
+            Some(metadata_section)
+        } else {
+            None
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        let mut valid = false;
+        match self.r#type {
+            TDX_METADATA_SECTION_TYPE_BFV => {
+                valid = self.raw_data_size != 0
+                    && self.memory_address != 0
+                    && self.memory_data_size != 0
+                    && self.attributes == TDX_METADATA_ATTRIBUTES_EXTENDMR;
+            }
+
+            TDX_METADATA_SECTION_TYPE_CFV => {
+                valid = self.raw_data_size != 0
+                    && self.memory_address != 0
+                    && self.memory_data_size != 0
+                    && self.attributes == 0;
+            }
+
+            TDX_METADATA_SECTION_TYPE_TD_HOB => {
+                valid = self.data_offset == 0
+                    && self.raw_data_size == 0
+                    && self.memory_address != 0
+                    && self.memory_data_size != 0
+                    && self.attributes == 0;
+            }
+
+            TDX_METADATA_SECTION_TYPE_TEMP_MEM => {
+                valid = self.data_offset == 0
+                    && self.raw_data_size == 0
+                    && self.memory_address != 0
+                    && self.memory_data_size != 0
+                    && self.attributes == 0;
+            }
+
+            TDX_METADATA_SECTION_TYPE_PAYLOAD => {
+                valid = self.data_offset == 0
+                    && self.raw_data_size == 0
+                    && self.memory_address != 0
+                    && self.memory_data_size != 0
+                    && self.attributes == 0;
+            }
+
+            TDX_METADATA_SECTION_TYPE_PAYLOAD_PARAM => {
+                valid = self.data_offset == 0
+                    && self.raw_data_size == 0
+                    && self.memory_address != 0
+                    && self.memory_data_size != 0
+                    && self.attributes == 0;
+            }
+
+            _ => {}
+        }
+        valid
+    }
+}
+
 #[repr(C)]
 #[derive(Pwrite, Pread)]
 pub struct TdxMetadataGuid {
-    pub data1: u32,
-    pub data2: u32,
-    pub data3: u32,
-    pub data4: u32,
+    pub guid: Guid,
 }
 
 impl TdxMetadataGuid {
     /// Check whether it's a valid
     pub fn is_valid(&self) -> bool {
-        self.data1 == TDX_METADATA_GUID1
-            && self.data2 == TDX_METADATA_GUID2
-            && self.data3 == TDX_METADATA_GUID3
-            && self.data4 == TDX_METADATA_GUID4
+        let metadata_guid = Guid::from_str(TDX_METADATA_GUID_STR).unwrap();
+        metadata_guid == self.guid
+    }
+
+    pub fn as_bytes(&self) -> &[u8; 16] {
+        self.guid.as_bytes()
+    }
+
+    /// Return TdxMetadataGuid based on the input buffer
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` - A buffer contains TdxMetadata guid.
+    pub fn from_bytes(buffer: &[u8; 16]) -> Option<TdxMetadataGuid> {
+        let guid = Guid::from_bytes(buffer);
+        let metadata_guid = TdxMetadataGuid { guid: guid };
+        if metadata_guid.is_valid() {
+            Some(metadata_guid)
+        } else {
+            None
+        }
     }
 }
 
 impl Default for TdxMetadataGuid {
     fn default() -> Self {
         TdxMetadataGuid {
-            data1: TDX_METADATA_GUID1,
-            data2: TDX_METADATA_GUID2,
-            data3: TDX_METADATA_GUID3,
-            data4: TDX_METADATA_GUID4,
+            guid: Guid::from_str(TDX_METADATA_GUID_STR).unwrap(),
         }
     }
 }
@@ -207,15 +341,8 @@ mod tests {
 
     #[test]
     fn test_tdx_metadata_guid() {
-        let mut guid = TdxMetadataGuid::default();
+        let guid = TdxMetadataGuid::default();
 
-        assert_eq!(guid.data1, TDX_METADATA_GUID1);
-        assert_eq!(guid.data2, TDX_METADATA_GUID2);
-        assert_eq!(guid.data3, TDX_METADATA_GUID3);
-        assert_eq!(guid.data4, TDX_METADATA_GUID4);
         assert_eq!(guid.is_valid(), true);
-
-        guid.data1 = 0;
-        assert_eq!(guid.is_valid(), false);
     }
 }
