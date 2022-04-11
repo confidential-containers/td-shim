06eb73ecbe6c59d53f17bd9cc692d8094a120edc
haowei <WeiX.Hao@intel.com>
2022-04-11 20:07:30 +0800

    cleanup the td-loader and td-uefi-pi fuzz
    Signed-off-by: haowei <WeiX.Hao@intel.com>

--- td-uefi-pi/fuzz/fuzz_targets/afl_payload_parser.rs
+++ td-uefi-pi/fuzz/fuzz_targets/afl_payload_parser.rs
@@ -12,38 +12,25 @@ fn main() {
         let mut args = std::env::args().skip(1);
         if let Some(arg) = args.next() {
             println!("{}", arg);
-            let data = std::fs::read(arg).expect("read crash file fail");
-            fuzz_payload_parser(data.as_slice());
-        } else {
-            let crashes_path = "../artifacts/afl_fv_parser/default/crashes";
-            let single_run = || {
-                let path = "../seeds/payload_parser/fv_buffer";
-                let data = std::fs::read(path).expect("read crash file fail");
+            let paths = std::path::Path::new(&arg);
+
+            if paths.is_file() {
+                let data = std::fs::read(&paths).expect("read crash file fail");
                 fuzz_payload_parser(data.as_slice());
-            };
-            // Read the crashes folder
-            match std::fs::read_dir(crashes_path) {
-                Ok(paths) => {
-                    // No files in the crashes folder
-                    if paths.count() == 0 {
-                        single_run();
-                        std::fs::remove_dir(crashes_path).unwrap();
-                    } else {
-                        println!("Run the crashes file in a loop...");
-                        for path in std::fs::read_dir(crashes_path).unwrap() {
-                            let path = &path.unwrap().path();
-                            if path.ends_with("README.txt") {
-                                continue;
-                            }
-                            let data = std::fs::read(path).expect("read crash file fail");
-                            fuzz_payload_parser(data.as_slice());
-                        }
+            }
+            else if paths.is_dir() {
+                for path in std::fs::read_dir(paths).unwrap() {
+                    let path = &path.unwrap().path();
+                    if path.ends_with("README.txt") {
+                        continue;
                     }
+
+                    let data = std::fs::read(path).expect("read crash file fail");
+                    fuzz_payload_parser(data.as_slice());
                 }
-                // The crashes folder does not exist, run single_run
-                Err(_) => {
-                    single_run();
-                }
+            }
+            else {
+                println!("No valid file path entered");
             }
         }
     }
