4f4f0456e6b8311cacf43328ad0df8ece113a42e
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-04-13 22:59:03 +0200

    sh_script: Add "pe with boot-kernel support" target
    pe with boot-kernel support is what's required for using td-shim with
    Cloud Hypervisor (standalone or with Kata Containers).
    
    In order to use the new target, just call the script as:
    ```sh
    build_final.sh pe_boot_kernel
    ``
    
    The result is a binary named `final-pe-boot-kernel.bin`.
    
    And it'll also the automatically whenever no arguments are passed to the
    `build_final.sh` script.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- sh_script/build_final.sh
+++ sh_script/build_final.sh
@@ -19,6 +19,17 @@ final_pe() {
         -o target/x86_64-unknown-uefi/release/final-pe.bin
 }
 
+final_pe_boot_kernel() {
+    echo "final-pe with boot-kernel support"
+    cargo xbuild -p td-shim --target x86_64-unknown-uefi --release --features=main,tdx,boot-kernel
+    cargo xbuild -p td-payload --target x86_64-unknown-uefi --release --features=main,tdx
+    cargo run -p td-shim-tools --features=td-shim/default,td-shim/tdx,boot-kernel --bin td-shim-ld -- \
+        target/x86_64-unknown-uefi/release/ResetVector.bin \
+        target/x86_64-unknown-uefi/release/td-shim.efi \
+        target/x86_64-unknown-uefi/release/td-payload.efi \
+        -o target/x86_64-unknown-uefi/release/final-pe-boot-kernel.bin
+}
+
 final_elf() {
     echo final-elf
     cargo xbuild -p td-shim --target x86_64-unknown-uefi --release --features=main,tdx --no-default-features
@@ -33,5 +44,6 @@ final_elf() {
 case "${1:-}" in
     elf) final_elf ;;
     pe) final_pe ;;
-    *) final_pe && final_elf ;;
+    pe_boot_kernel) final_pe_boot_kernel ;;
+    *) final_pe && final_pe_boot_kernel && final_elf ;;
 esac
