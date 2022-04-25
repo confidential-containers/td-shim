6112cc63284752963045979e9102216276e122de
Wei Liu <wei3.liu@intel.com>
2022-04-26 01:25:57 +0800

    tests: Add more checkpoints for testacpi
    Add more checkpoints: signature revision oem_id
    oem_table_id creator_id and checksum
    
    Signed-off-by: Wei Liu <wei3.liu@intel.com>

--- tests/test-td-payload/src/testacpi.rs
+++ tests/test-td-payload/src/testacpi.rs
@@ -15,14 +15,12 @@ use td_layout::memslice;
 use td_shim::acpi::GenericSdtHeader;
 use td_shim::TD_ACPI_TABLE_HOB_GUID;
 use td_uefi_pi::hob;
-use zerocopy::FromBytes;
+use zerocopy::{AsBytes, FromBytes};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TdAcpiData {
     pub name: String,
     pub signature: [u8; 4],
-    pub valid: u8,
-    pub exist: u8,
 }
 
 impl Default for TdAcpiData {
@@ -30,8 +28,6 @@ impl Default for TdAcpiData {
         TdAcpiData {
             name: String::default(),
             signature: [0, 0, 0, 0],
-            valid: 0,
-            exist: 0,
         }
     }
 }
@@ -63,8 +59,8 @@ impl TestTdAcpi {
         (255 - data.iter().fold(0u8, |acc, x| acc.wrapping_add(*x))).wrapping_add(1)
     }
 
-    fn parse_hob(&self, hob_address: usize) -> Vec<TdAcpiData> {
-        let mut tables: Vec<TdAcpiData> = Vec::new();
+    fn parse_hob(&self, hob_address: usize) -> Vec<GenericSdtHeader> {
+        let mut tables: Vec<GenericSdtHeader> = Vec::new();
 
         // Parse Hob to populate td_acpi_list
         let hob_buffer = unsafe {
@@ -78,19 +74,20 @@ impl TestTdAcpi {
         while let Some(hob) =
             hob::get_next_extension_guid_hob(next_hob, TD_ACPI_TABLE_HOB_GUID.as_bytes())
         {
-            let table = hob::get_guid_data(hob).expect("Failed to get data from ACPI GUID HOB");
+            let table = hob::get_guid_data(hob).expect("Failed to get data from ACPI GUID HOB\n");
             let header = GenericSdtHeader::read_from(&table[..size_of::<GenericSdtHeader>()])
-                .expect("Faile to read table header from ACPI GUID HOB");
+                .expect("Faile to read table header from ACPI GUID HOB\n");
 
-            let mut tbl = TdAcpiData {
-                name: String::from_utf8(header.signature.to_ascii_uppercase()).unwrap(),
-                signature: header.signature,
-                valid: 1,
-                exist: 1,
-            };
-
+            // Check checksum
+            if self.calculate_checksum(table) != 0 {
+                log::info!(
+                    "ACPI checksum {} is not correct\n",
+                    String::from_utf8_lossy(&header.signature)
+                );
+                return tables;
+            }
             // save it to tables
-            tables.push(tbl);
+            tables.push(header);
 
             // Then we go to next hob
             next_hob = hob::seek_to_next_hob(hob).unwrap();
@@ -99,35 +96,61 @@ impl TestTdAcpi {
         return tables;
     }
 
-    fn verify_tables(&self, acpi_tables: Vec<TdAcpiData>) -> TestResult {
+    fn verify_tables(&self, acpi_tables: Vec<GenericSdtHeader>) -> TestResult {
         if acpi_tables.len() == 0 {
-            log::info!("Not find ACPI tables in Hob\n");
+            log::info!("Not find ACPI tables in Hob or no ACPI tables found with valid checksum\n");
+            return TestResult::Fail;
+        }
+
+        if acpi_tables.len() != self.td_acpi.expected.num as usize {
+            log::info!(
+                "Not find all ACPI tables in Hob, expected {}\n",
+                self.td_acpi.expected.num as usize
+            );
             return TestResult::Fail;
         }
 
         let mut cnt: usize = 0;
-        while cnt < self.td_acpi.expected.num as usize {
+        while cnt < acpi_tables.len() {
             let expected_signature = self.td_acpi.expected.tables[cnt].signature;
-
             let index = acpi_tables
                 .iter()
                 .position(|r| r.signature == expected_signature);
             // if the ACPI is not found
             if index.is_none() {
                 log::info!(
-                    "ACPI {} is not found.",
+                    "ACPI {} is not found.\n",
                     String::from_utf8_lossy(&expected_signature)
                 );
                 return TestResult::Fail;
             }
 
-            // valid?
             let idx = index.unwrap();
 
-            if acpi_tables[idx].valid == 0 {
-                // This table is not valid
+            if acpi_tables[idx].revision != 1
+                || acpi_tables[idx].oem_id != *b"INTEL "
+                || acpi_tables[idx].oem_table_id != u64::from_le_bytes(*b"SHIM    ")
+                || acpi_tables[idx].creator_id != u32::from_le_bytes(*b"SHIM")
+            {
                 log::info!(
-                    "ACPI {} is not valid.",
+                    "Expected revision: 1          Actual revision: {:?}\n",
+                    acpi_tables[idx].revision
+                );
+                log::info!(
+                    "Expected oem_id: 'Intel'      Actual oem_id: {:?}\n",
+                    String::from_utf8_lossy(&acpi_tables[idx].oem_id)
+                );
+                log::info!(
+                    "Expected oem_table_id: 'SHIM' Actual oem_table_id: {:?}\n",
+                    String::from_utf8_lossy(acpi_tables[idx].oem_table_id.as_bytes())
+                );
+                log::info!(
+                    "Expected creator_id: 'SHIM'   Actual creator_id: {:?}\n",
+                    String::from_utf8_lossy(&acpi_tables[idx].creator_id.as_bytes())
+                );
+
+                log::info!(
+                    "ACPI {} is not valid.\n",
                     String::from_utf8_lossy(&expected_signature)
                 );
                 return TestResult::Fail;
