96861dec1564c3036b863adfa554a27ae73317de
Jiaqi Gao <jiaqi.gao@intel.com>
2023-02-14 02:32:02 -0500

    td-shim-tools: update `layout-builder` to generate td-shim runtime layout
    Add time in memory configuration file `runtime.json` to indicate whether the
    memory should be placed at the high-end or the low-end of low memory.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- td-shim-tools/src/bin/td-shim-layout-builder/main.rs
+++ td-shim-tools/src/bin/td-shim-layout-builder/main.rs
@@ -2,15 +2,15 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 
+use std::path::PathBuf;
+
 use clap::{Parser, ValueEnum};
-use td_shim_tools::layout_builder::{self, parse_json, parse_layout_config, MemoryRegions};
+use td_shim_tools::layout_builder::parse_json;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum ConfigType {
-    /// json format config
-    JSON,
-    /// json compatible with td-layout-config
-    LayoutConfig,
+    /// Memory layout config
+    Memory,
 }
 
 #[derive(Parser)]
@@ -19,7 +19,7 @@ struct Cli {
     /// Memory config file pathname.
     config: String,
     /// Specify config file type, e.g. json, layout-config
-    #[clap(short = 't', long = "config_type", value_enum, default_value_t = ConfigType::LayoutConfig)]
+    #[clap(short = 't', long = "config_type", value_enum, default_value_t = ConfigType::Memory)]
     config_type: ConfigType,
     /// Memory base address.
     #[clap(short = 'b', long = "base", default_value_t = String::from("0x0"))]
@@ -35,23 +35,22 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    let base = usize::from_str_radix(cli.base.trim_start_matches("0x"), 16)
-        .expect("input address is not valid");
-    let memory_regions = MemoryRegions::new(base);
+    let config = std::fs::read_to_string(cli.config.to_string())
+        .expect("Content is configuration file is invalid");
 
-    let memory_regions = match cli.config_type {
-        ConfigType::JSON => parse_json::parse_memory(memory_regions, cli.config),
-        ConfigType::LayoutConfig => parse_layout_config::parse_memory(memory_regions, cli.config),
+    let output_file = cli.output.as_ref().map(|path| PathBuf::from(&path));
+
+    match cli.config_type {
+        ConfigType::Memory => output(&cli, parse_json::parse_memory(config), output_file),
     };
+}
 
-    let ret = layout_builder::render(&memory_regions).expect("Render memory layout failed!");
-
+fn output(cli: &Cli, data: String, dir: Option<PathBuf>) {
     if cli.print_flag {
-        print!("{}", ret);
+        print!("{}", data);
     }
 
-    if cli.output.is_some() {
-        let output = cli.output.unwrap();
-        std::fs::write(output, &ret).expect("write file error")
+    if let Some(path) = dir {
+        std::fs::write(path, data.as_bytes()).expect("Failed to create image layout file");
     }
 }
--- td-shim-tools/src/layout_builder/mod.rs
+++ td-shim-tools/src/layout_builder/mod.rs
@@ -3,9 +3,5 @@
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 
 pub mod parse_json;
-pub mod parse_layout_config;
 mod region;
 mod render;
-
-pub use region::MemoryRegions;
-pub use render::render;
