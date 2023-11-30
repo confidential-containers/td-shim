26bb5d0cc3badbea05ea1fef8d4cd6c9919588c5
Jiaqi Gao <jiaqi.gao@intel.com>
2023-11-30 03:23:53 -0500

    deps/ring: upgrade to `0.17.6`
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- sh_script/preparation.sh
+++ sh_script/preparation.sh
@@ -3,7 +3,7 @@
 preparation() {
     # apply the patch set for ring
     pushd library/ring
-    git reset --hard c3fda8b4dd57d658923c397c6cfaa33591f6f256
+    git reset --hard 464d367252354418a2c17feb806876d4d89a8508
     git clean -f -d
     patch -p 1 -i ../patches/ring.diff
     popd
