95d7a24a2e5961a674ee6b768eccd07753e5b566
Jiaqi Gao <jiaqi.gao@intel.com>
2024-05-10 03:27:39 -0400

    devtools/td-layout-config: support large payload
    The payload may be so large that it cannot be placed in the payload
    region of the original image config.
    
    To solve this, `LargePayload` is introduced. Since the large payload
    may have size larger than 16MiB, it is loaded into physical memory instead
    of ROM space. Thus, in td-layout-config tool, the image configuration is
    splited into rom configuration and image configuration.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- devtools/td-layout-config/src/image.rs
+++ devtools/td-layout-config/src/image.rs
@@ -2,8 +2,13 @@ use serde::Deserialize;
 
 use super::{layout::LayoutConfig, render};
 
+const ROM_BASE: usize = 0xFF00_0000;
+const ROM_SIZE: usize = 0x100_0000;
+
 #[derive(Deserialize, Debug, PartialEq)]
 struct ImageConfig {
+    #[serde(rename = "LargePayload")]
+    large_payload: Option<String>,
     #[serde(rename = "Config")]
     config: String,
     #[serde(rename = "Mailbox")]
@@ -28,60 +33,66 @@ pub fn parse_image(data: String) -> String {
     let image_config = serde_json::from_str::<ImageConfig>(&data)
         .expect("Content is configuration file is invalid");
 
-    let mut image_layout = LayoutConfig::new(0, 0x100_0000);
-    image_layout.reserve_low(
-        "Config",
-        parse_int::parse::<u32>(&image_config.config).unwrap() as usize,
-        "Reserved",
-    );
-    image_layout.reserve_low(
-        "Mailbox",
-        parse_int::parse::<u32>(&image_config.mailbox).unwrap() as usize,
-        "Reserved",
-    );
-    image_layout.reserve_low(
-        "TempStack",
-        parse_int::parse::<u32>(&image_config.temp_stack).unwrap() as usize,
-        "Reserved",
-    );
-    image_layout.reserve_low(
-        "TempHeap",
-        parse_int::parse::<u32>(&image_config.temp_heap).unwrap() as usize,
-        "Reserved",
-    );
+    let large_payload_size = image_config
+        .large_payload
+        .map(|large_payload| parse_int::parse::<u32>(&large_payload).unwrap() as usize);
+    let config_size = parse_int::parse::<u32>(&image_config.config).unwrap() as usize;
+    let mailbox_size = parse_int::parse::<u32>(&image_config.mailbox).unwrap() as usize;
+    let temp_stack_size = parse_int::parse::<u32>(&image_config.temp_stack).unwrap() as usize;
+    let temp_heap_size = parse_int::parse::<u32>(&image_config.temp_heap).unwrap() as usize;
+    let reset_vector_size = parse_int::parse::<u32>(&image_config.reset_vector).unwrap() as usize;
+    let bootloader_size = parse_int::parse::<u32>(&image_config.bootloader).unwrap() as usize;
+    let metadata_size = parse_int::parse::<u32>(&image_config.metadata).unwrap() as usize;
+    let td_info_size = image_config
+        .td_info
+        .map(|td_info| parse_int::parse::<u32>(&td_info).unwrap() as usize);
+    let payload_size = image_config
+        .builtin_payload
+        .map(|payload| parse_int::parse::<u32>(&payload).unwrap() as usize);
 
-    image_layout.reserve_high(
-        "ResetVector",
-        parse_int::parse::<u32>(&image_config.reset_vector).unwrap() as usize,
-        "Reserved",
-    );
-    image_layout.reserve_high(
-        "Ipl",
-        parse_int::parse::<u32>(&image_config.bootloader).unwrap() as usize,
-        "Reserved",
-    );
+    let image_size = large_payload_size.unwrap_or(0)
+        + config_size
+        + mailbox_size
+        + temp_stack_size
+        + temp_heap_size
+        + reset_vector_size
+        + bootloader_size
+        + metadata_size
+        + td_info_size.unwrap_or(0)
+        + payload_size.unwrap_or(0);
 
-    image_layout.reserve_high(
-        "Metadata",
-        parse_int::parse::<u32>(&image_config.metadata).unwrap() as usize,
-        "Reserved",
-    );
-
-    if let Some(td_info_config) = image_config.td_info {
-        image_layout.reserve_high(
-            "TdInfo",
-            parse_int::parse::<u32>(&td_info_config).unwrap() as usize,
-            "Reserved",
-        )
+    let mut image_layout = LayoutConfig::new(0, image_size);
+    if let Some(size) = large_payload_size {
+        image_layout.reserve_low("LargePayload", size, "Reserved")
+    }
+    image_layout.reserve_low("Config", config_size, "Reserved");
+    image_layout.reserve_low("Mailbox", mailbox_size, "Reserved");
+    image_layout.reserve_low("TempStack", temp_stack_size, "Reserved");
+    image_layout.reserve_low("TempHeap", temp_heap_size, "Reserved");
+    image_layout.reserve_high("ResetVector", reset_vector_size, "Reserved");
+    image_layout.reserve_high("Ipl", bootloader_size, "Reserved");
+    image_layout.reserve_high("Metadata", metadata_size, "Reserved");
+    if let Some(size) = td_info_size {
+        image_layout.reserve_high("TdInfo", size, "Reserved")
+    }
+    if let Some(size) = payload_size {
+        image_layout.reserve_high("Payload", size, "Reserved")
     }
 
-    if let Some(payload_config) = image_config.builtin_payload {
-        image_layout.reserve_high(
-            "Payload",
-            parse_int::parse::<u32>(&payload_config).unwrap() as usize,
-            "Reserved",
-        )
+    let mut rom_layout = LayoutConfig::new(ROM_BASE, ROM_BASE + ROM_SIZE);
+    rom_layout.reserve_low("Config", config_size, "Reserved");
+    rom_layout.reserve_low("Mailbox", mailbox_size, "Reserved");
+    rom_layout.reserve_low("TempStack", temp_stack_size, "Reserved");
+    rom_layout.reserve_low("TempHeap", temp_heap_size, "Reserved");
+    rom_layout.reserve_high("ResetVector", reset_vector_size, "Reserved");
+    rom_layout.reserve_high("Ipl", bootloader_size, "Reserved");
+    rom_layout.reserve_high("Metadata", metadata_size, "Reserved");
+    if let Some(size) = td_info_size {
+        rom_layout.reserve_high("TdInfo", size, "Reserved")
+    }
+    if let Some(size) = payload_size {
+        rom_layout.reserve_high("Payload", size, "Reserved")
     }
 
-    render::render_image(&image_layout).expect("Render image layout failed!")
+    render::render_image(&image_layout, &rom_layout).expect("Render image layout failed!")
 }
