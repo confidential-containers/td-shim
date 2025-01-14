2346dd5f2687050ad88bf89e5f2314c8795d978f
Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>
2025-01-14 10:10:10 +0100

    Revert "Fix compilation warning in ring library"
    This reverts commit 186f94feb5fb87578b0b3901b82737e76634d4ef.
    
    Signed-off-by: Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>

--- sh_script/preparation.sh
+++ sh_script/preparation.sh
@@ -5,11 +5,6 @@ preparation() {
     pushd library/ring
     git reset --hard 464d367252354418a2c17feb806876d4d89a8508
     git clean -f -d
-
-    # apply the patch to get rid of unused import warning during compilation
-    # https://github.com/briansmith/ring/commit/c4742e0cae849f08ff410a817c5266af41670b3d
-    git cherry-pick c4742e0cae849f08ff410a817c5266af41670b3d
-
     patch -p 1 -i ../patches/ring.diff
     popd
 }
