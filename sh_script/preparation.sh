e1aa1f9aa09a46b83d2212ffea114021256669d6
Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>
2025-01-14 09:10:08 +0100

    Fix compilation warning in ring library The following warning is being fixed:
    warning: unused imports: `PKCS1`, `PSS`
      --> ring/src/rsa/padding.rs:21:13
       |
    21 |     pkcs1::{PKCS1, RSA_PKCS1_SHA256, RSA_PKCS1_SHA384, RSA_PKCS1_SHA512},
       |             ^^^^^
    22 |     pss::{PSS, RSA_PSS_SHA256, RSA_PSS_SHA384, RSA_PSS_SHA512},
       |           ^^^
       |
       = note: `#[warn(unused_imports)]` on by default
    
    That warning was already fixed with this patch:
    briansmith/ring@c4742e0
    
    Once ring library version will be bumped up, this patch could be dropped.
    
    Signed-off-by: Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>

--- sh_script/preparation.sh
+++ sh_script/preparation.sh
@@ -6,6 +6,7 @@ preparation() {
     git reset --hard 464d367252354418a2c17feb806876d4d89a8508
     git clean -f -d
     patch -p 1 -i ../patches/ring.diff
+    git apply ../patches/0001-NFC-Address-Clippy-unused-import-warning.patch
     popd
 }
 
