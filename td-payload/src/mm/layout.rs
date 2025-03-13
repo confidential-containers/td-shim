8d7d69dbe92fe2bb076873db72252f6ff830e30d
Stanislaw Grams <stanislaw.grams@intel.com>
2025-03-13 15:28:15 +0100

    td-payload: introduce no-shared-mem feature
    Signed-off-by: Stanislaw Grams <stanislaw.grams@intel.com>

--- td-payload/src/mm/layout.rs
+++ td-payload/src/mm/layout.rs
@@ -5,6 +5,7 @@
 pub const DEFAULT_HEAP_SIZE: usize = 0x1000000;
 pub const DEFAULT_STACK_SIZE: usize = 0x800000;
 pub const DEFAULT_PAGE_TABLE_SIZE: usize = 0x800000;
+#[cfg(not(feature = "no-shared-mem"))]
 pub const DEFAULT_SHARED_MEMORY_SIZE: usize = 0x100000;
 #[cfg(feature = "cet-shstk")]
 pub const DEFAULT_SHADOW_STACK_SIZE: usize = 0x10000;
@@ -14,6 +15,7 @@ pub struct RuntimeLayout {
     pub heap_size: usize,
     pub stack_size: usize,
     pub page_table_size: usize,
+    #[cfg(not(feature = "no-shared-mem"))]
     pub shared_memory_size: usize,
     #[cfg(feature = "cet-shstk")]
     pub shadow_stack_size: usize,
@@ -25,6 +27,7 @@ impl Default for RuntimeLayout {
             heap_size: DEFAULT_HEAP_SIZE,
             stack_size: DEFAULT_STACK_SIZE,
             page_table_size: DEFAULT_PAGE_TABLE_SIZE,
+            #[cfg(not(feature = "no-shared-mem"))]
             shared_memory_size: DEFAULT_SHARED_MEMORY_SIZE,
             #[cfg(feature = "cet-shstk")]
             shadow_stack_size: DEFAULT_SHADOW_STACK_SIZE,
