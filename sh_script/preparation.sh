31d30b5b224c2db5e7b08b097f0384e8d5050713
Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>
2025-03-13 15:26:21 +0100

    Bump ring library version to 0.17.14
    Following items included:
    * ring library bumping up from 0.17.6 to 0.17.14
    * dropping patch for NFC: Address Clippy unused import warning introduced
    with https://github.com/confidential-containers/td-shim/pull/760
    * dropping SSE and SSE2 checks
    
    Signed-off-by: Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>

--- sh_script/preparation.sh
+++ sh_script/preparation.sh
@@ -3,10 +3,10 @@
 preparation() {
     # apply the patch set for ring
     pushd library/ring
-    git reset --hard 464d367252354418a2c17feb806876d4d89a8508
+    git reset --hard 2723abbca9e83347d82b056d5b239c6604f786df
     git clean -f -d
-    patch -p 1 -i ../patches/ring.diff
-    git apply ../patches/0001-NFC-Address-Clippy-unused-import-warning.patch
+    git apply ../patches/ring.diff
+    git apply ../patches/0002-Disable-checks-for-SSE-and-SSE2.patch
     popd
 }
 
