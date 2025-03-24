0d8defefbfda097d96c44565c760add44ff428a3
Mikko Ylinen <mikko.ylinen@intel.com>
2025-03-24 15:11:05 +0200

    td-shim-tee-info-hash: rework build_mrtd()
    Help code readers to understand what is going on. The rework
    was a by-product of issue #740 debugging.
    
    The rework removes the unnecessary temp buffer copies/zeroing
    and moves to use .chunks_exact() splitting of the data. Moreover,
    don't hide any file seek/read errors but panic if they occur.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- td-shim-tools/src/tee_info_hash.rs
+++ td-shim-tools/src/tee_info_hash.rs
@@ -155,6 +155,14 @@ impl fmt::Display for TdInfoStruct {
     }
 }
 
+const MEM_PAGE_ADD: [u8; 16] = [
+    b'M', b'E', b'M', b'.', b'P', b'A', b'G', b'E', b'.', b'A', b'D', b'D', 0, 0, 0, 0,
+];
+const MR_EXTEND: [u8; 16] = [
+    b'M', b'R', b'.', b'E', b'X', b'T', b'E', b'N', b'D', 0, 0, 0, 0, 0, 0, 0,
+];
+const MRTD_EXTENSION_BUFFER_PADDING: [u8; 104] = [0; 104];
+
 impl TdInfoStruct {
     pub fn pack(self, buffer: &mut [u8; size_of::<TdInfoStruct>()]) -> usize {
         buffer.zeroize();
@@ -283,10 +291,6 @@ impl TdInfoStruct {
 
         desc_offset += size_of::<TdxMetadataDescriptor>();
 
-        let mut buffer128: [u8; MRTD_EXTENSION_BUFFER_SIZE] = [0; MRTD_EXTENSION_BUFFER_SIZE]; // used by page add
-        let mut buffer3_128: [[u8; MRTD_EXTENSION_BUFFER_SIZE]; 3] =
-            [[0; MRTD_EXTENSION_BUFFER_SIZE]; 3]; // used by mr extend
-
         let mut sha384hasher = Sha384::new();
 
         for _i in 0..descriptor.number_of_section_entry {
@@ -320,37 +324,49 @@ impl TdInfoStruct {
                 panic!("Invalid type value!\n");
             }
 
-            let nr_pages = sec.memory_data_size / PAGE_SIZE;
+            raw_image_file
+                .seek(SeekFrom::Start(sec.data_offset as u64))
+                .expect("Seek cursor to sec.data_offset");
 
-            for iter in 0..nr_pages {
+            let mut section_data = vec![0u8; sec.raw_data_size as usize];
+
+            raw_image_file
+                .read_exact(&mut section_data)
+                .expect("Read from sec.data_offset");
+
+            section_data.resize_with(sec.memory_data_size as usize, Default::default);
+
+            let mut page_addr = sec.memory_address;
+
+            for page in section_data.chunks_exact(PAGE_SIZE as usize) {
+                // Use TDCALL [TDH.MEM.PAGE.ADD]
                 if sec.attributes & TDX_METADATA_ATTRIBUTES_EXTEND_MEM_PAGE_ADD == 0 {
-                    // Use TDCALL [TDH.MEM.PAGE.ADD]
-                    fill_buffer128_with_mem_page_add(
-                        &mut buffer128,
-                        sec.memory_address + iter * PAGE_SIZE,
-                    );
-
-                    sha384hasher.update(buffer128);
+                    // Byte 0 through 15 contain the ASCII string 'MEM.PAGE.ADD' and padding.
+                    sha384hasher.update(MEM_PAGE_ADD);
+                    // Byte 16 through 23 contain the GPA (in little-endian format).
+                    sha384hasher.update(page_addr.to_le_bytes());
+                    // 0 padding to 128 byte buffer.
+                    sha384hasher.update(MRTD_EXTENSION_BUFFER_PADDING);
                 }
 
-                // check attributes
+                // Use TDCALL [TDH.MR.EXTEND]
                 if sec.attributes & TDX_METADATA_ATTRIBUTES_EXTENDMR != 0 {
-                    // Use TDCALL [TDH.MR.EXTEND]
-                    let granularity = TDH_MR_EXTEND_GRANULARITY;
-                    let iteration = PAGE_SIZE / granularity;
-                    for chunk_iter in 0..iteration {
-                        fill_buffer3_128_with_mr_extend(
-                            &mut buffer3_128,
-                            sec.memory_address + iter * PAGE_SIZE + chunk_iter * granularity,
-                            raw_image_file,
-                            sec.data_offset as u64 + iter * PAGE_SIZE + chunk_iter * granularity,
-                        );
+                    let mut chunk_addr = page_addr;
 
-                        sha384hasher.update(buffer3_128[0]);
-                        sha384hasher.update(buffer3_128[1]);
-                        sha384hasher.update(buffer3_128[2]);
+                    for chunk in page.chunks_exact(TDH_MR_EXTEND_GRANULARITY as usize) {
+                        // Byte 0 through 15 contain the ASCII string 'MR.EXTEND' and padding.
+                        sha384hasher.update(MR_EXTEND);
+                        // Byte 16 through 23 contain the GPA (in little-endian format).
+                        sha384hasher.update(chunk_addr.to_le_bytes());
+                        // 0 padding to 128 byte buffer.
+                        sha384hasher.update(MRTD_EXTENSION_BUFFER_PADDING);
+
+                        // Hash 256 bytes of chunk data
+                        sha384hasher.update(chunk);
+                        chunk_addr += TDH_MR_EXTEND_GRANULARITY;
                     }
                 }
+                page_addr += PAGE_SIZE;
             }
         }
         let hash = sha384hasher.finalize();
@@ -488,28 +504,6 @@ fn fill_buffer128_with_mem_page_add(buf: &mut [u8; MRTD_EXTENSION_BUFFER_SIZE], 
     buf[0..MEM_PAGE_ADD_ASCII_SIZE].copy_from_slice("MEM.PAGE.ADD".as_bytes());
     buf[MEM_PAGE_ADD_GPA_OFFSET..MEM_PAGE_ADD_GPA_OFFSET + MEM_PAGE_ADD_GPA_SIZE]
         .copy_from_slice(gpa.to_le_bytes().as_ref());
-}
-
-fn fill_buffer3_128_with_mr_extend(
-    buf: &mut [[u8; MRTD_EXTENSION_BUFFER_SIZE]; 3],
-    gpa: u64,
-    file: &mut File,
-    data_offset: u64,
-) {
-    buf[0].zeroize();
-    buf[1].zeroize();
-    buf[2].zeroize();
-
-    // Byte 0 through 8 contain the ASCII string 'MR.EXTEND'.
-    // Byte 16 through 23 contain the GPA (in little-endian format).
-    // All the other bytes contain 0. The other two extension buffers contain the chunk’s content.
-    buf[0][0..MR_EXTEND_ASCII_SIZE].copy_from_slice("MR.EXTEND".as_bytes());
-    buf[0][MR_EXTEND_GPA_OFFSET..MR_EXTEND_GPA_OFFSET + MR_EXTEND_GPA_SIZE]
-        .copy_from_slice(gpa.to_le_bytes().as_ref());
-
-    file.seek(SeekFrom::Start(data_offset)).unwrap();
-    file.read_exact(&mut buf[1]).unwrap();
-    file.read_exact(&mut buf[2]).unwrap();
 }
 
 fn fill_buffer3_128_with_mr_extend_tdvf(
